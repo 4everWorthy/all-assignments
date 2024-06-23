7d187ab1dd5d204f30312e604bb536b3e17797f7
Jason Worthy <109769584+4everWorthy@users.noreply.github.com>
2024-06-23 01:38:04 -0400

    2nd commit

--- travering-the-DOM/script.js
+++ travering-the-DOM/script.js
@@ -14,11 +14,14 @@ const currentSection = document.querySelector('.current');
     // Calls document.querySelector method with .current as an argument
 console.log(currentSection)
 
+if (currentSection.nextElementSibling) { // adding 'if/else' statement corrected dev console error.
 const nextSection = currentSection.nextElementSibling;
 // The nextElementSibling property is used to get the next sibling element of the current section.
     //  It directly accesses the next element in the DOM that is at the same hierarchical level.
 console.log(nextSection)
-
+} else {
+    console.log("No next sibling section found");
+}
 const previousSectionHeader = currentSection.previousElementSibling.querySelector('h2');
 // The previousElementSibling property is used to get the previous sibling element of the current section.
     // The querySelector('h2') method is then used to select the <h2> element within that previous section.
@@ -29,14 +32,19 @@ const containingDiv = document.querySelector('h2.highlight').closest('div');
     // The closest('div') method is then used to find the nearest ancestor <div> element containing that <h2>.
 console.log(containingDiv)
 
-const sectionWithH2 = Array.from(document.querySelectorAll('section')).filter(section => section.querySelector('h2'));
+const sectionsWithH2 = document.querySelectorAll('section h2');
+// console.log(sectionWithH2) ... This corrected the console error I was seeing but not sure if this is correct.
+
 // This consolidates the retrieval of all sections with headers
     // The querySelectorAll method can be used with a CSS pseudo-class to select all <section> elements that contain an <h2> element.
         // This is a convenient way to filter elements based on their descendants.
-console.log(sectionWithH2)
+// console.log(sectionWithH2) ... This corrected the console error I was seeing but not sure if this is correct.
 
 
+//Dev console... Uncaught ReferenceError: sectionWithH2 is not defined
+    // at script.js:36:13
 
+    // script.js:39 Uncaught ReferenceError: sectionWithH2 is not defined    at script.js:39:13
 
-        
+// The if statement checks if currentSection is not null before trying to use it. This prevents errors that happen when you try to do something with an element that doesn't exist.       
 
